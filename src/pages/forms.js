85fcbf98fbae103ba93e0f80808c80723c106e61
Rafeeq Weideman <r.w.weideman.rafeeq@gmail.com>
2022-11-15 15:18:15 +0000

    forms update

--- src/pages/forms.js
+++ src/pages/forms.js
@@ -4,6 +4,23 @@ import './forms.css';
 
 function DoctorForm() {
 
+    function nextBtn() {
+        var step1 = document.getElementById("Tab");
+        if (step1.style.display === "block") {
+            step1.style.display = "none";
+        } else {
+            step1.style.display = "none";
+        }
+    }
+
+    function validateForm() {
+        let x = document.forms["myForm"]["fname"].value;
+        if (x == "") {
+          alert("Name must be filled out");
+          return false;
+        }
+      }
+
     return (
         <div className="DoctorsForm">
             {/* Navigation */}
@@ -13,7 +30,7 @@ function DoctorForm() {
             {/* Navigation */}
 
             <div className='FormArea'>
-                <form className='Form'>
+                <form className='Form' name='myForm' onSubmit={validateForm} >
 
                     <div className='FormHeader'>
                         <div className='TopLeft'>
@@ -34,63 +51,113 @@ function DoctorForm() {
                         and click the submit button.
                     </div>
 
-                    <div className='PersonalInformation'>
+                    <div className='Tab' id='Tab'>
+                        <div className='PersonalInformation'>
 
-                        <div className='Header-Droplist'>
-                            <div className='SectionHeader'>PERSONAL INFORMATION</div>
-                            <div>
-
+                            <div className='Header-Droplist'>
+                                <div className='SectionHeader'>PERSONAL INFORMATION</div>
                                 <div>
-                                    <label for='doctors'>Please specify:</label>
-                                    <select id='type'>
-                                        <option value='practioner'>General Practioner</option>
-                                        <option value='OB/GYN'>Obstetrician and Gynaecologist</option>
-                                        <option value='psychiatrist'>Psychiatrist</option>
-                                        <option value='Dentist'>Dentist</option>
-                                        <option value='surgeon'>General Surgeon</option>
-                                        <option value='dermatologist'>Dermatologist</option>
-                                    </select>
+
+                                    <div>
+                                        <label for='doctors'>Please specify:</label>
+                                        <select id='type'>
+                                            <option value='practioner'>General Practioner</option>
+                                            <option value='OB/GYN'>Obstetrician and Gynaecologist</option>
+                                            <option value='psychiatrist'>Psychiatrist</option>
+                                            <option value='Dentist'>Dentist</option>
+                                            <option value='surgeon'>General Surgeon</option>
+                                            <option value='dermatologist'>Dermatologist</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
+
+                            <div className='Input1'>
+                                <input type="text" placeholder='Name' required />
+                                <input type="text" placeholder='Application Date' required />
+                            </div>
+                            <div className='Input2'>
+                                <div><input type="text" placeholder='Surname' required /></div>
+                                <div><input type="text" placeholder='Address' required /></div>
+                            </div>
+
+                            <div className='Input3'>
+                                <input type="text" placeholder='City' required />
+                                <input type="text" placeholder='Province' required />
+                                <input type="text" placeholder='Postal Code' required />
+                                <input type="text" placeholder='Country' required />
+                            </div>
+
+                            <div className='Input4'><input type="text" placeholder='Email Address' required /></div>
+
+                            <div className='Input5'>
+                                <input type="text" placeholder='Cell' required />
+                                <input type="text" placeholder='Telephone' required />
+                                <input type="text" placeholder='Fax' required />
+                            </div>
+
+                            <div className='Input6'>
+                                <input type="text" placeholder='ID Number' required />
+                                <input type="text" placeholder='Citizenship' required />
+                            </div>
+                            <div className='Input7'><input type="text" placeholder='Languages Spoken' required /></div>
                         </div>
-
-                        <div className='Input1'>
-                            <input type="text" placeholder='Name' required />
-                            <input type="text" placeholder='Application Date' required />
-                        </div>
-                        <div className='Input2'>
-                            <div><input type="text" placeholder='Surname' required /></div>
-                            <div><input type="text" placeholder='Address' required /></div>
-                        </div>
-
-                        <div className='Input3'>
-                            <input type="text" placeholder='City' required />
-                            <input type="text" placeholder='Province' required />
-                            <input type="text" placeholder='Postal Code' required />
-                            <input type="text" placeholder='Country' required />
-                        </div>
-
-                        <div className='Input4'><input type="text" placeholder='Email Address' required /></div>
-
-                        <div className='Input5'>
-                            <input type="text" placeholder='Cell' required />
-                            <input type="text" placeholder='Telephone' required />
-                            <input type="text" placeholder='Fax' required />
-                        </div>
-
-                        <div className='Input6'>
-                            <input type="text" placeholder='ID Number' required />
-                            <input type="text" placeholder='Citizenship' required />
-                        </div>
-
-                        <div className='Input7'><input type="text" placeholder='Languages Spoken' required /></div>
-
                     </div>
 
-                    <div className='Input8'><input type='submit' ></input></div>
+                    <div className='Tab1' id='Tab1'>
+                        <div className='ProfessionalInformation'>
+                            <div className='SectionHeader1'>PROFESSIONAL INFORMATION</div>
+
+                            <div>
+                                <div><input className='JobTitle' type='text' placeholder='Current Job Title' /></div>
+                            </div>
+                            <div className='Edu'>Education</div>
+                            <div className='Education'>
+
+                                <div>
+                                    <input type='text' placeholder='Institution' /><br></br>
+                                    <input type='text' placeholder='Programmer(if applicable):' />
+                                </div>
+                                <div>
+                                    <input type='text' placeholder='Starting Date:' /><br></br>
+                                    <input type='text' placeholder='Ending Date:' />
+                                </div>
+                            </div>
+                            <div className='Edu'>Medical Education</div>
+                            <div className='MedicalEducation'>
+                                <div>
+                                    <input type='text' placeholder='Institution' /><br></br>
+                                    <input type='text' placeholder='Programmer(if applicable):' />
+                                </div>
+                                <div>
+                                    <input type='text' placeholder='Starting Date:' /><br></br>
+                                    <input type='text' placeholder='Ending Date:' />
+                                </div>
+                            </div>
+
+                            <div className='Input5'>
+                                <input type='text' placeholder='Medical License Number:' />
+                                <input type='text' placeholder='Country:' />
+                                <input type='text' placeholder='Date Issued:' />
+                            </div>
+                            <div>
+                                <div className='SectionText'>
+                                    This site and all its information is protected by the POPI Act of South Africa
+                                    and all the information you provided will be handled according to the POPI Act,
+                                    to continue your application please agree to the terms.
+                                </div>
+                                <div className='CheckBox'><input type='checkbox' />I hereby agree to the terms.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='Input8'>
+                        {/* <button type='button' onClick={preBtn}>Back</button> */}
+                        <button type='submit'>Next</button>
+                        <button type='submit' onClick={nextBtn}>Next</button>
+                    </div>
                 </form>
             </div>
-            <div className='PageNum'>Page 1/3</div>
+            {/* <div className='PageNum'>Page 1/3</div> */}
         </div>
     )
 }
