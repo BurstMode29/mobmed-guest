8ff3263bb942f4fbeb362378a1cdd15ea20544c6
maluleke-jabulani <malulekejabulan96@gmail.com>
2023-01-27 10:37:19 +0200

    firebase

--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -7,30 +7,6 @@ import Polygon from '../images/polygon.png'
 import { useState } from 'react';
 
 function LoginForm() {
-<<<<<<< HEAD
-    // firebase
-const auth = getAuth(app); 
-
-    
-const [email, setEmail] = useState("")
-const [password, setPassword] = useState("")
-const signIn = () =>{
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        console.log(user);
-        alert("SignIn succesfully ")
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        // const errorMessage = error.message;
-        alert(errorCode)
-      });
-// firebase
-=======
 // firebase
 const auth = getAuth(app); 
 
@@ -54,7 +30,6 @@ const auth = getAuth(app);
           });
 // firebase
 
->>>>>>> e427bc6ca6c5ba98ef3b25da8b5f3d65cfee91c4
     return (
         <div className='LoginForm'>
             {/* Navigation */}
@@ -70,11 +45,7 @@ const auth = getAuth(app);
                     <div>
                         <div className='LoginFormHeader'>LOGIN</div>
                         <div className='LoginFormText'>Welcome back!</div>
-<<<<<<< HEAD
-                        <input type='email' placeholder='User ID' onChange={(e) => setEmail(e.target.value)} /><br></br>
-=======
                         <input type='email' placeholder='User ID' onChange={(e) => setEmail(e.target.value)}/><br></br>
->>>>>>> e427bc6ca6c5ba98ef3b25da8b5f3d65cfee91c4
                         <input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
                         <div className='Forgot'>Forgot Password?..</div>
                         <button type='submit' onClick={signIn}>LOGIN</button>
